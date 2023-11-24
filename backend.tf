7b1c3a768f5a52238a911b0fb6e0f773240a8576
root <root@ip-172-31-11-240.us-east-2.compute.internal>
2023-11-24 09:06:33 +0000

    modded vpc file

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     region = "us-east-2"
     bucket = "terraform-backyend-jenkins"
-    key = "terraform"
+    key    = "terraform"
   }
 }
--- vpc-sg.tf
+++ vpc-sg.tf
@@ -1,30 +1,30 @@
 resource "aws_vpc" "vpc-terra" {
-  cidr_block = "192.168.0.0/16"
+  cidr_block       = "192.168.0.0/16"
   instance_tenancy = "default"
-  tags =  {
+  tags = {
     Name = "terra-vpc"
   }
 }
 
-resource "aws_security_groups" "sg-1"  {
-  Name = "sg-1"
-  vpc_id = aws_vpc.vpc-terra.id
-  Description = "sg for vpc-terra"
+resource "aws_security_group" "sg-1" {
+  name        = "ec-sg-1"
+  vpc_id      = aws_vpc.vpc-terra.id
+  description = "sg for vpc-terra"
   ingress {
-    From_port = "22"
-    To_port = "22"
-    protocol = "tcp"
-    cidr_block = "0.0.0.0/0"
+    from_port  = "22"
+    to_port    = "22"
+    protocol   = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    From_port = "0"
-    To_port = "65535"
-    protocol = "-1"
-    cidr_block = "0.0.0.0/0"
+    from_port  = "0"
+    to_port    = "65535"
+    protocol   = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-   Name = "sg-1"
+    Name = "ec2-sg"
   }
 }
